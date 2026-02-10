ab533d749014bb35e9fe76c14f46e5c4123f5a41
pahocsapatrik-max <pahocsa.patrik@gmail.com>
2026-02-10 22:11:35 +0100

    Add files via upload

---
+++ clocky/clocky/logout.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+// Minden session változó ürítése
+$_SESSION = array();
+
+// Ha van session süti, azt is érdemes érvényteleníteni
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// A szerver oldali session megsemmisítése
+session_destroy();
+
+// Irány a kezdőlap
+header('Location: index.php');
+exit();
+?>
